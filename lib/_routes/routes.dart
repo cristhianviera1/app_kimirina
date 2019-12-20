6353115005ccb44e060bc22b80bcb63fa26441a3
Alanys Rojas <afv.rojas@yavirac.edu.ec>
2019-12-19 23:07:03 -0500

    agregar páginas nuevas

--- lib/_routes/routes.dart
+++ lib/_routes/routes.dart
@@ -7,10 +7,7 @@ const String resetPasswordViewRoute = 'reset_password';
 const String homeViewRoute = 'home';
 const String chatDetailsViewRoute = 'chat_details';
 const String userDetailsViewRoute = 'user_details';
-<<<<<<< HEAD
 
 const String riesgoViewRoute = 'riesgo';
 
-=======
 const String selectOptionItsRoute = 'select_option_its';
->>>>>>> 27e373bdda3373a3845e7dfc81fc679715257384
