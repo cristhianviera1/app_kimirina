cdcfb5938f2b3f22dbced8b1d989d5bf24875f7d
Gabriel <cristhian.viera1@gmail.com>
2019-12-19 22:43:24 -0500

    landingPage

---
+++ lib/models/slide.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Slide {
+  final String imageUrl;
+  final String animationName;
+  final String title;
+  final String description;
+
+  Slide({
+    @required this.imageUrl,
+    @required this.title,
+    @required this.animationName,
+    @required this.description,
+  });
+}
+
+final slideList = [
+  Slide(
+    imageUrl: 'assets/animations/Lock_Animation.flr',
+    animationName: 'Untitled',
+    title: 'Kimirina',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tincidunt bibendum. Maecenas eu viverra orci. Duis diam leo, porta at justo vitae, euismod aliquam nulla.',
+  ),
+  Slide(
+    imageUrl: 'assets/animations/Lock_Animation.flr',
+    animationName: 'Confianza',
+    title: 'Confianza',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tincidunt bibendum. Maecenas eu viverra orci. Duis diam leo, porta at justo vitae, euismod aliquam nulla.',
+  ),
+  Slide(
+    imageUrl: 'assets/animations/Lock_Animation.flr',
+    animationName: 'Untitled',
+    title: 'Confidencialidad',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tincidunt bibendum. Maecenas eu viverra orci. Duis diam leo, porta at justo vitae, euismod aliquam nulla.',
+  ),
+];
